2d90a6dcd7c6a53f812f0c462096e3ce87c24b98
lucas-cid <lucas_mcid@hotmail.com>
2021-11-19 10:56:13 -0300

    Remove a materia criada para exemplo

--- navigation/tabs/CalendarNavigator.js
+++ navigation/tabs/CalendarNavigator.js
@@ -19,42 +19,41 @@ let mode = 0;
 
 export default function CalendarNavigator() {
     const events = useSelector(state => state.events.events);
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     
-    useEffect(() => {
-        const newEvent = {
-            "id": 5,
-            "weekly": true,
-            "details": [
-                {
-                    "datetime_init": "1995-12-17T12:24:00-03:00",
-                    "datetime_end": "1995-12-17T15:02:00-03:00",
-                    "local": "ava2",
-                    "day": 5
-                }
-            ],
-            "name": "AED10",
-            "subject": "AED10",
-            "notification": [
-                120,
-                60,
-                5
-            ],
-            "description": "aula do fulano",
-            "color": "#00f",
-            "is_subject": true,
-            "mean": "(P0 + P1 + P2)/3",
-            "grade": {
-                "P0": 0,
-                "P1": 5,
-                "P2": null
-            },
-            "frequence": {}
-        }
+    // useEffect(() => {
+    //     const newEvent = {
+    //         "id": 5,
+    //         "weekly": true,
+    //         "details": [
+    //             {
+    //                 "datetime_init": "1995-12-17T12:24:00-03:00",
+    //                 "datetime_end": "1995-12-17T15:02:00-03:00",
+    //                 "local": "ava2",
+    //                 "day": 5
+    //             }
+    //         ],
+    //         "name": "AED10",
+    //         "subject": "AED10",
+    //         "notification": [
+    //             120,
+    //             60,
+    //             5
+    //         ],
+    //         "description": "aula do fulano",
+    //         "color": "#00f",
+    //         "is_subject": true,
+    //         "mean": "(P0 + P1 + P2)/3",
+    //         "grade": {
+    //             "P0": 0,
+    //             "P1": 5,
+    //             "P2": null
+    //         },
+    //         "frequence": {}
+    //     }
 
-        dispatch(addEvent(newEvent));
-    }, [])
-    
+    //     dispatch(addEvent(newEvent));
+    // }, [])
     return (
         <calendarStack.Navigator screenOptions={{
             presentation: 'containedTransparentModal', animation: "fade"
