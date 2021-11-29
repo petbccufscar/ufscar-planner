4260a0c05b58b9d2be8b5e6459f655874daac8f9
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2021-11-29 16:59:04 -0300

    Removendo componentes obsoletos

--- navigation/tabs/CalendarNavigator.js
+++ navigation/tabs/CalendarNavigator.js
@@ -1,20 +1,15 @@
-import React, { useEffect } from 'react';
-import { Button } from 'react-native-paper';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { CalendarDay } from "../../components/CalendarDay";
-import { CalendarMonth } from '../../components/CalendarMonth';
-import { CalendarWeek } from "../../components/CalendarWeek";
-import { IconButton, Colors } from 'react-native-paper';
+import React from 'react';
+import { Colors, IconButton } from 'react-native-paper';
 // import { events } from '../../placeholder-data/data';
-import { useSelector, useDispatch } from 'react-redux';
-import { addEvent } from '../../redux/actions/eventActions';
+import { useSelector } from 'react-redux';
+import {Calendar} from '../../components/Calendar'
 
 const calendarStack = createNativeStackNavigator();
 
 const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 const hoje = new Date();
 let mode = 0;
-//let titulo = mode == 2 ? `${meses[hoje.getMonth()]} de ${hoje.getFullYear()}` : "Calendário"
 
 
 export default function CalendarNavigator() {
@@ -58,53 +53,12 @@ export default function CalendarNavigator() {
         <calendarStack.Navigator screenOptions={{
             presentation: 'containedTransparentModal', animation: "fade"
         }}>
-            <calendarStack.Screen name="CalendarDay" options={({ navigation }) => ({
+            <calendarStack.Screen name="Calendar" options={({ navigation }) => ({
                 title: `${meses[hoje.getMonth()]} de ${hoje.getFullYear()}`,
-                headerRight: () => (
-                    <IconButton
-                        icon="calendar"
-                        color={Colors.black}
-                        size={20}
-                        onPress={() => {
-                            navigation.replace("CalendarMonth", { tasks: events })
-                        }}
-                    />
-                ),
             })}>
-                {props => <CalendarDay {...props} tasks={events}/>}
+                {props => <Calendar/>}
             </calendarStack.Screen>
 
-            <calendarStack.Screen name="CalendarMonth" options={({ navigation }) => ({
-                title: `${meses[hoje.getMonth()]} de ${hoje.getFullYear()}`,
-                headerRight: () => (
-                    <IconButton
-                        icon="calendar"
-                        color={Colors.black}
-                        size={20}
-                        onPress={() => {
-                            navigation.replace("CalendarWeek", { tasks: events })
-                        }}
-                    />
-                ),
-            })}>
-                {props => <CalendarMonth {...props} tasks={events}/>}
-            </calendarStack.Screen>
-
-            <calendarStack.Screen name="CalendarWeek" options={({ navigation }) => ({
-                title: `${meses[hoje.getMonth()]} de ${hoje.getFullYear()}`,
-                headerRight: () => (
-                    <IconButton
-                        icon="calendar"
-                        color={Colors.black}
-                        size={20}
-                        onPress={() => {
-                            navigation.replace("CalendarDay", { tasks: events })
-                        }}
-                    />
-                ),
-            })}>
-                    {props => <CalendarWeek {...props} tasks={events}/>}
-            </calendarStack.Screen>
         </calendarStack.Navigator>
     );
 }
