ff9dab379412c55653363a5c16b0e6672cf4b435
lucas-cid <lucas_mcid@hotmail.com>
2021-11-30 17:23:06 -0300

    Implementaçao da tela de visualizaçao de materias

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SideBar from './navigation/SideBar';
-import BottomNavBar from './navigation/BottomNavBar';
-import { createStackNavigator } from '@react-navigation/stack';
-import EditEvent from './components/EditEvent';
-import { NavigationContainer } from '@react-navigation/native';
-import {Provider} from 'react-redux';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Platform, UIManager } from "react-native";
+import SideBar from "./navigation/SideBar";
+import { createStackNavigator } from "@react-navigation/stack";
+import Event from "./components/Event";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+
+if (
+  Platform.OS === "android" &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 const HomeStackRoutes = createStackNavigator();
 
@@ -20,8 +26,18 @@ export default function App() {
           <HomeStackRoutes.Navigator>
             <HomeStackRoutes.Group screenOptions={{ headerShown: false }}>
               <HomeStackRoutes.Screen name="SideBar" component={SideBar} />
-              </HomeStackRoutes.Group>
-            <HomeStackRoutes.Screen name="EditEvent" component={EditEvent}/>
+            </HomeStackRoutes.Group>
+            <HomeStackRoutes.Screen
+              name="Event"
+              component={Event}
+              options={() => ({
+                headerStyle: {
+                  backgroundColor: "#e8243c",
+                },
+                headerTintColor: "#fff",
+                headerTitleAlign: "center",
+              })}
+            />
           </HomeStackRoutes.Navigator>
         </NavigationContainer>
       </PersistGate>
@@ -32,8 +48,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
