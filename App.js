b022d393d0eb88ccfc0b0e26a5c4c24cdfb81110
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2022-03-27 15:09:40 -0300

    Separando temas
    Co-Authored-By: João Bueno <37675609+jobucaldas@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,114 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState, useRef } from "react";
 import {
-  StyleSheet,
-  Platform,
-  UIManager,
-  View,
-  Button,
-  Text,
-} from "react-native";
-import SideBar from "./navigation/SideBar";
+  NavigationContainer
+} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Event from "./components/Event";
+import * as Notifications from "expo-notifications";
+import React, { useEffect, useRef } from "react";
+import {
+  Platform, StyleSheet, UIManager
+} from "react-native";
+import {
+  Provider as PaperProvider
+} from "react-native-paper";
+import Toast from "react-native-toast-message";
 import {
   Provider as ReduxProvider,
   useDispatch,
-  useSelector,
+  useSelector
 } from "react-redux";
-import { store } from "./redux/store";
-import { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { loadEvents } from "./redux/actions/eventActions";
+import Event from "./components/Event";
 import { Subject } from "./components/Subject";
+import SideBar from "./navigation/SideBar";
+import { loadEvents } from "./redux/actions/eventActions";
+import { persistor, store } from "./redux/store";
 import Restaurant from "./screens/Restaurant";
-import * as Notifications from "expo-notifications";
-import Toast from "react-native-toast-message";
 import { PreferencesContext } from "./theme/PreferencesContext";
+import {CombinedDefaultTheme, CombinedDarkTheme} from "./theme/Themes";
 
-import {
-  NavigationContainer,
-  DarkTheme as NavigationDarkTheme,
-  DefaultTheme as NavigationDefaultTheme,
-} from "@react-navigation/native";
-import {
-  DarkTheme as PaperDarkTheme,
-  DefaultTheme as PaperDefaultTheme,
-  Provider as PaperProvider,
-} from "react-native-paper";
 
-const CombinedDefaultTheme = {
-  ...PaperDefaultTheme,
-  ...NavigationDefaultTheme,
-  colors: {
-    ...PaperDefaultTheme.colors,
-    ...NavigationDefaultTheme.colors,
-    primary: "#BB1822",
-    onPrimary: "#FFFFFF",
-    primaryContainer: "#FFDAD5",
-    onPrimaryContainer: "#410003",
-    secondary: "#C00103",
-    onSecondary: '#FFFFFF',
-    secondaryContainer: '#FFDAD3',
-    onSecondaryContainer: '#410000',
-    tertiary: '#725B2E',
-    onTertiary: '#FFFFFF',
-    tertiaryContainer: '#FFDEA6',
-    onTertiaryContainer: '#271900',
-    error: '#BA1B1B',
-    onError: '#FFFFFF',
-    errorContainer: '#FFDAD4',
-    onErrorContainer: '#410001',
-    background: '#FBFDFD',
-    onBackground: '#191C1D',
-    surface: '#FBFDFD',
-    onSurface: '#191C1D',
-    surfaceVariant: '#F4DDDB',
-    onSurfaceVariant: '#524342',
-    outline: '#857371',
-    surface1: '#F8F2F2',
-    surface2: '#F6EBEB',
-    surface3: '#F4E4E5',
-    surface4: '#F3E2E3',
-    surface5: '#F2DDDE'
-  },
-};
-const CombinedDarkTheme = {
-  ...PaperDarkTheme,
-  ...NavigationDarkTheme,
-  colors: {
-    ...PaperDarkTheme.colors,
-    ...NavigationDarkTheme.colors,
-    primary: '#FFB3AB',
-    onPrimary: '#680008',
-    primaryContainer: '#930010',
-    onPrimaryContainer: '#FFDAD5',
-    secondary: '#FFB4A7',
-    onSecondary: '#680000',
-    secondaryContainer: '#930000',
-    onSecondaryContainer: '#FFDAD3',
-    tertiary: '#E1C28C',
-    onTertiary: '#402D05',
-    tertiaryContainer: '#584319',
-    onTertiaryContainer: '#FFDEA6',
-    error: '#FFB4A9',
-    onError: '#680003',
-    errorContainer: '#930006',
-    onErrorContainer: '#FFDAD4', 
-    background: '#191C1D',
-    onBackground: '#E0E3E3',
-    surface: '#191C1D',
-    onSurface: '#E0E3E3',
-    surfaceVariant:  '#524342',
-    onSurfaceVariant: '#D7C1BF',
-    outline: '#A08C8A',
-    surface1: '#242424',
-    surface2: '#2B2828',
-    surface3: '#322D2D',
-    surface4: '#352E2E',
-    surface5: '#393131'
-  },
-};
 
 if (
   Platform.OS === "android" &&
