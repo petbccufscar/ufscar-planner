7624719add3602813c3439222b08e03b4d908737
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2022-05-04 00:37:29 -0300

    Tematizando a agenda

--- components/Calendar.js
+++ components/Calendar.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { InteractionManager, View, StyleSheet } from "react-native";
 import { Agenda, LocaleConfig, CalendarProps } from 'react-native-calendars';
 import { useSelector } from 'react-redux';
-import { Task as CalendarTask } from './CalendarTask';
+import { CalendarTask } from './CalendarTask';
 import { FAB } from 'react-native-paper';
 import { useNavigation } from "@react-navigation/core";
 import Toast from 'react-native-toast-message';
 import { defaultTask, floorDate } from '../helpers/helper';
+import { useTheme } from "react-native-paper";
 
 export function Calendar() {
 
@@ -58,17 +59,44 @@ function EventsScreen() {
   };
   const navigation = useNavigation()
   const rowHasChanged = (r1, r2) => r1 !== r2;
-  
+  const th = useTheme()
+  const colors = th.colors;
+
 
   return (
     <>
       <Agenda
+        key={th.dark}
         items={stItems}
         selected={new Date()}
         renderItem={renderItem}
         renderEmptyDate={renderEmptyDate}
         rowHasChanged={rowHasChanged}
         markedDates={stMarked}
+
+        theme={
+          {
+            selectedDayBackgroundColor: colors.primary, // primary
+            todayTextColor: colors.primary,// primary
+            dayTextColor: colors.outline, //outline
+            backgroundColor: colors.surface1,//surface1
+            monthTextColor: colors.onPrimaryContainer,
+            textDisabledColor: colors.outline, // outline
+            agendaKnobColor: colors.primary, // primary
+            textSectionTitleColor: colors.onPrimaryContainer, //on primary container
+            textDayHeaderFontWeight: 'bold',
+            selectedDayTextColor: colors.onPrimary, //on primary container
+            calendarBackground: colors.background,//background
+          
+            dayTextColor: colors.outline,//outline
+            agendaTodayColor: colors.primary,
+            agendaDayTextColor: colors.outline,
+            agendaDayNumColor: colors.outline,
+          }
+        }
+        
+      
+
       />
       <FAB
         style={styles.fab}
