2155997598d2d1a3ae611533694838011f292cc3
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2023-03-06 20:08:03 +0000

    Documenta a tela do aviso importante

--- screens/RestaurantNotice.tsx
+++ screens/RestaurantNotice.tsx
@@ -3,12 +3,35 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import ScrollView from "../components/ScrollView";
 import { useTheme } from "react-native-paper";
 
+/**
+ * Um aviso sobre o restaurante universitário.
+ */
+type Notice = {
+  /**
+   * O título do aviso.
+   */
+  title: string,
+
+  /**
+   * Uma descrição curta sobre o conteúdo do aviso.
+   */
+  description: string,
+
+  /**
+   * Uma string contendo um ou mais parágrafos com o conteúdo do aviso.
+   */
+  content: string,
+};
+
 type RootStackParamList = {
-  RestaurantNotice: { content: string },
+  RestaurantNotice: Notice,
 };
 
 type Props = NativeStackScreenProps<RootStackParamList, "RestaurantNotice">;
 
+/**
+ * Tela que apresenta o conteúdo de um aviso sobre o restaurante universitário.
+ */
 export default function RestaurantNotice({ route }: Props) {
   const theme = useTheme();
   const { content } = route.params;
